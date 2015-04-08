6e3da22c3d3d85c8758d3f18cda130b451c4621e
Olga Gwenu <poluektova.ol@gmail.com>
2015-04-08 18:50:36 +0300

    [Minor changes]: Added comment to model fields.

--- src/main/java/ag/processmonitor/models/SystemProcess.java
+++ src/main/java/ag/processmonitor/models/SystemProcess.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public class SystemProcess {
 	
-	private int pid;
+	private int pid;		//Process identifier
 	private String user;
-	private double cpu;
+	private double cpu;		// Central processing unit
 	private double mem;
-	private int vsz;
-	private int rrs;
-	private String tty; //Possibly should be Enum
-	private String stat; //Possibly should be Enum 
+	private int vsz;		// Virtual Memory Size. It includes all memory that the process can access, including memory that is swapped out and memory that is from shared libraries.
+	private int rrs;		// Resident Set Size and is used to show how much memory is allocated to that process and is in RAM. It does not include memory that is swapped out. 
+	private String tty; 	// Teletype Possibly -> Possibly should be Enum
+	private String stat; 	// Possibly should be Enum 
 	private Date start;
 	private Date time;
 	private String command;
@@ -19,66 +19,87 @@ public class SystemProcess {
 	public int getPid() {
 		return pid;
 	}
+
 	public void setPid(int pid) {
 		this.pid = pid;
 	}
+
 	public String getUser() {
 		return user;
 	}
+
 	public void setUser(String user) {
 		this.user = user;
 	}
+
 	public double getCpu() {
 		return cpu;
 	}
+
 	public void setCpu(double cpu) {
 		this.cpu = cpu;
 	}
+
 	public double getMem() {
 		return mem;
 	}
+
 	public void setMem(double mem) {
 		this.mem = mem;
 	}
+
 	public int getVsz() {
 		return vsz;
 	}
+
 	public void setVsz(int vsz) {
 		this.vsz = vsz;
 	}
+
 	public int getRrs() {
 		return rrs;
 	}
+
 	public void setRrs(int rrs) {
 		this.rrs = rrs;
 	}
+
 	public String getTty() {
 		return tty;
 	}
+
 	public void setTty(String tty) {
 		this.tty = tty;
 	}
+
 	public String getStat() {
 		return stat;
 	}
+
 	public void setStat(String stat) {
 		this.stat = stat;
 	}
+
 	public Date getStart() {
 		return start;
 	}
+
 	public void setStart(Date start) {
 		this.start = start;
 	}
+
 	public Date getTime() {
 		return time;
 	}
+
 	public void setTime(Date time) {
 		this.time = time;
 	}
+
 	public String getCommand() {
 		return command;
 	}
+
 	public void setCommand(String command) {
 		this.command = command;
 	}
