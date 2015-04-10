569cd4f4a911d4a534c8caae3f7af303260b22b4
Olga Gwenu <poluektova.ol@gmail.com>
2015-04-10 11:48:45 +0300

    [Implementation]: Added obj builder to SystemProcess class.

--- src/main/java/ag/processmonitor/models/SystemProcess.java
+++ src/main/java/ag/processmonitor/models/SystemProcess.java
@@ -1,5 +1,7 @@
 package ag.processmonitor.models;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SystemProcess {
@@ -9,7 +11,7 @@ public class SystemProcess {
 	private double cpu;		// Central processing unit
 	private double mem;
 	private int vsz;		// Virtual Memory Size. It includes all memory that the process can access, including memory that is swapped out and memory that is from shared libraries.
-	private int rrs;		// Resident Set Size and is used to show how much memory is allocated to that process and is in RAM. It does not include memory that is swapped out. 
+	private int rss;		// Resident Set Size and is used to show how much memory is allocated to that process and is in RAM. It does not include memory that is swapped out. 
 	private String tty; 	// Teletype Possibly -> Possibly should be Enum
 	private String stat; 	// Possibly should be Enum 
 	private Date start;
@@ -20,87 +22,148 @@ public class SystemProcess {
 		return pid;
 	}
 
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-
 	public String getUser() {
 		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
 	}
 
 	public double getCpu() {
 		return cpu;
 	}
 
-	public void setCpu(double cpu) {
-		this.cpu = cpu;
-	}
-
 	public double getMem() {
 		return mem;
-	}
-
-	public void setMem(double mem) {
-		this.mem = mem;
 	}
 
 	public int getVsz() {
 		return vsz;
 	}
 
-	public void setVsz(int vsz) {
-		this.vsz = vsz;
-	}
-
-	public int getRrs() {
-		return rrs;
-	}
-
-	public void setRrs(int rrs) {
-		this.rrs = rrs;
+	public int getRss() {
+		return rss;
 	}
 
 	public String getTty() {
 		return tty;
 	}
 
-	public void setTty(String tty) {
-		this.tty = tty;
-	}
-
 	public String getStat() {
 		return stat;
-	}
-
-	public void setStat(String stat) {
-		this.stat = stat;
 	}
 
 	public Date getStart() {
 		return start;
 	}
 
-	public void setStart(Date start) {
-		this.start = start;
-	}
-
 	public Date getTime() {
 		return time;
-	}
-
-	public void setTime(Date time) {
-		this.time = time;
 	}
 
 	public String getCommand() {
 		return command;
 	}
-
-	public void setCommand(String command) {
-		this.command = command;
+	
+	@Override
+	public String toString() {
+		DateFormat timeFormater = new SimpleDateFormat("HH:mm");
+		StringBuilder sb = new StringBuilder();
+		sb.append("ag.processmonitor.models.SystemProcess ");
+		sb.append("[user = " + user + "]");
+		sb.append("[pid = " + pid + "]");
+		sb.append("[cpu = " + cpu + "]");
+		sb.append("[mem = " + mem + "]");
+		sb.append("[vsz = " + vsz + "]");
+		sb.append("[rss = " + rss + "]");
+		sb.append("[tty = " + tty + "]");
+		sb.append("[stat = " + stat + "]");
+		sb.append("[start = " + timeFormater.format(start) + "]");
+		sb.append("[time = " + timeFormater.format(time) + "]");
+		sb.append("[command = " + command + "]");
+		return sb.toString();
+	}
+	
+	public static class Builder {
+		private int pid;	
+		private String user;
+		private double cpu;	
+		private double mem;
+		private int vsz;
+		private int rss;
+		private String tty;
+		private String stat;
+		private Date start;
+		private Date time;
+		private String command;
+		
+		public Builder pid(int pid) {
+			this.pid = pid;
+			return this;
+		}
+		
+		public Builder user(String user) {
+			this.user = user;
+			return this;
+		}
+		
+		public Builder cpu(double cpu) {
+			this.cpu = cpu;
+			return this;
+		}
+		
+		public Builder mem(double mem) {
+			this.mem = mem;
+			return this;
+		}
+		
+		public Builder vsz(int vsz) {
+			this.vsz = vsz;
+			return this;
+		}
+		
+		public Builder rss(int rss) {
+			this.rss = rss;
+			return this;
+		}
+		
+		public Builder tty(String tty) {
+			this.tty = tty;
+			return this;
+		}
+		public Builder stat(String stat) {
+			this.stat = stat;
+			return this;
+		}
+		
+		public Builder start(Date start) {
+			this.start = start;
+			return this;
+		}
+		
+		public Builder time(Date time) {
+			this.time = time;
+			return this;
+		}
+		
+		public Builder command(String command) {
+			this.command = command;
+			return this;
+		}
+		
+		public SystemProcess build() {
+			return new SystemProcess(this);
+		}
+	}
+	
+	private SystemProcess(Builder builder) {
+		this.pid = builder.pid;	
+		this.user = builder.user;
+		this.cpu = builder.cpu;	
+		this.mem = builder.mem;
+		this.vsz = builder.vsz;
+		this.rss = builder.rss;
+		this.tty = builder.tty;
+		this.stat = builder.stat;
+		this.start = builder.start;
+		this.time = builder.time;
+		this.command = builder.command;
 	}
 }
--- src/main/java/ag/processmonitor/services/impl/UnixProcessReceiver.java
+++ src/main/java/ag/processmonitor/services/impl/UnixProcessReceiver.java
@@ -2,7 +2,13 @@ package ag.processmonitor.services.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 import ag.processmonitor.models.SystemProcess;
 import ag.processmonitor.services.ProcessReceivable;
@@ -15,19 +21,89 @@ public class UnixProcessReceiver implements ProcessReceivable {
 		    // Execute command
 		    String command = "ps aux";
 		    Process child = Runtime.getRuntime().exec(command);
-
+		    
 		    // Get the input stream and read from it
 		    InputStream in = child.getInputStream();
 		    
-		    // TODO: Implement mapper to SystemProcess and then print obj
-		    int c;
-		    while ((c = in.read()) != -1) {
-		        System.out.print((char)c);
-		    }
-		    in.close();
+		    parseCommandResult(in);
 		} catch (IOException e) {
 		}
 		return null;
 	}
+	
+	private void parseCommandResult(InputStream in) {
+		boolean firstLine = true;
+        Scanner scanner = new Scanner(in);
+         
+        //read file line by line
+        scanner.useDelimiter(System.getProperty("line.separator"));
+        while(scanner.hasNext()){
+        	if(firstLine) {
+        		firstLine = false;
+        		scanner.nextLine();
+        	} 
+        	
+        	SystemProcess process = parseLine(scanner.next());
+        	System.out.println(process.toString());
+        }
+        scanner.close();
+	}
+	
+	private SystemProcess parseLine(String line) {
+		Scanner scanner = new Scanner(line);
+        scanner.useDelimiter("\\s+");
+        
+        String userValue = scanner.next();
+        int pidValue = getIntValue(scanner.next());
+        double cpuValue = getDoubleValue(scanner.next());
+        double memValue = getDoubleValue(scanner.next());
+        int vszValue = getIntValue(scanner.next());
+        int rssValue = getIntValue(scanner.next());
+        String ttyValue = scanner.next();
+        String statValue = scanner.next();
+        
+        Date startValue = null;
+        Date timeValue = null;
+        DateFormat startTime = new SimpleDateFormat("HH:mm");
+        
+        try {
+			startValue = startTime.parse(scanner.next());
+			timeValue = startTime.parse(scanner.next());
+		} catch (ParseException e) {
+			// TODO: 
+		}
+        
+        String commandValue = scanner.next();
+        scanner.close();
+        
+        SystemProcess process = new SystemProcess.Builder().user(userValue).pid(pidValue).cpu(cpuValue)
+        		.mem(memValue).vsz(vszValue).rss(rssValue).tty(ttyValue).stat(statValue).start(startValue)
+        		.time(timeValue).command(commandValue).build();
+        
+        return process;
+   }
+	
+	private int getIntValue(String strValue) {
+		int value = -1;
 
+		try {
+			value = Integer.parseInt(strValue);
+		} catch (InputMismatchException exception) {
+			// TODO:
+		}
+
+		return value;
+	}
+
+	private double getDoubleValue(String strValue) {
+		double value = -1.0;
+
+		try {
+			value = Double.parseDouble(strValue);
+		} catch (InputMismatchException exception) {
+			// TODO:
+		}
+
+		return value;
+	}
 }
